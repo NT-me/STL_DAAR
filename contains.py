c8f4c73ab5102252ae8a23c8b61c104a73aba7f0
gnouf1 <jeuxforum26@gmail.com>
2021-09-27 23:55:37 +0200

    Start modif on parser

--- contains.py
+++ contains.py
@@ -1,30 +1,30 @@
 from regextree import RegExTree as Ret
-
+from utils import specialValues as sv
 
 def containAltern(trees):
     for t in trees:
-        if t.root == '|' and not t.subTrees:
+        if t.root == sv.ALTERNATE and not t.subTrees:
             return True
     return False
 
 
 def containParenthese(trees):
     for t in trees:
-        if t.root == '(' or t.root == ')':
+        if t.root == sv.PARENTHESEOUVERTE or t.root == sv.PARENTHESEFERMEE:
             return True
     return False
 
 
 def containEtoile(trees):
     for t in trees:
-        if t.root == "*" and not t.subTrees:
+        if t.root == sv.STAR and not t.subTrees:
             return True
     return False
 
 
 def containPlus(trees):
     for t in trees:
-        if t.root == "+" and not t.subTrees:
+        if t.root == sv.PLUS and not t.subTrees:
             return True
     return False
 
@@ -33,11 +33,11 @@ def containConcat(trees):
     flagFound = False
     # print(trees)
     for t in trees:
-        if not flagFound and t.root != '|':
+        if not flagFound and t.root != sv.ALTERNATE:
             flagFound = True
             continue
         if flagFound:
-            if t.root != '|':
+            if t.root != sv.ALTERNATE:
                 return True
             else:
                 flagFound = False
--- utils.py
+++ utils.py
@@ -3,3 +3,9 @@ from enum import Enum
 
 class specialValues(Enum):
     PROTECTION = "###PROTECTION###"
+    CONCAT = "###CONCAT###"
+    ALTERNATE = "###ALTERNATE###"
+    PLUS = "###PLUS###"
+    STAR = "###STAR###"
+    PARENTHESEOUVERTE = "###PARENTHESEOUVERTE###"
+    PARENTHESEFERMEE = "###PARENTHESEFERMEE###"
