b5cc32a429d2c243fa26ccd4dc0919c08ed00580
NTheo <jeuxforum26@gmail.com>
2021-10-09 23:33:50 +0200

    New feature : -t

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import regextreeToAutomaton as rta
 import determinization as deter
 import printer as printer
 import sys
+import time as t
+
 
 if __name__ == '__main__':
 
@@ -14,10 +16,14 @@ if __name__ == '__main__':
     fileName = sys.argv[2]
 
     viewMode = False
+    timeMode = False
 
     if len(sys.argv) > 3:
-        if sys.argv[3] == '-v' or sys.argv[3] == '-view':
+        # if sys.argv[3] == '-v' or sys.argv[3] == '-view':
+        if "-v" in sys.argv or "--view" in sys.argv:
             viewMode = True
+        if "-t" in sys.argv or "--time" in sys.argv:
+            timeMode = True
         else:
             print("Error : invalid option -- " + "'" + sys.argv[3] + "'")
             sys.exit(1)
@@ -30,20 +36,48 @@ if __name__ == '__main__':
 
     # inputRegex = input("Entrez une String ou une Regex:\n")
 
+    if timeMode:
+        genStart = t.time()
+        timedict = dict()
+
     ast = r.preparse(inputRegex)
+
+    if timeMode:
+        timedict["Parsing : "] = t.time() - genStart
 
     if ast.isWord():
         printer.egrep(0, file, ast)
+        if timeMode:
+            timedict["KMP : "] = t.time() - genStart
 
     else:
         automatonNDFA = rta.toAutomaton(ast)
+
+        if timeMode:
+            timedict["AST -> Automaton : "] = t.time() - genStart
+
         automatonDFA = deter.deter(automatonNDFA)
+
+        if timeMode:
+            timedict["Deter : "] = t.time() - genStart
+
         if viewMode:
             automatonDFA.goToMermaid("deter")
         automatonDFA.mini()
+
+        if timeMode:
+            timedict["Mini : "] = t.time() - genStart
+
         if viewMode:
             automatonDFA.goToMermaid("mini")
         printer.egrep(1, file, automatonDFA)
+
+    if timeMode:
+        timedict["Text Matching : "] = t.time() - genStart
+        timedict["General : "] = t.time() - genStart
+        print("\n== TIME ==\n")
+        for et in timedict:
+            print("{0} {1}\n".format(et, timedict[et]))
 
     if viewMode:
 
